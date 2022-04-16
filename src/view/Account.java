9fd29674fbc3cfa31372b8a29fd76aa4620a0260
unknown <you@example.com>
2022-04-16 17:06:10 +0700

    first commit

--- src/view/Account.java
+++ src/view/Account.java
@@ -15,7 +15,6 @@ public class Account {
     Scanner sc = new Scanner(System.in);
 
 
-
     public void Register() {
         int id;
         if (staffList.size() == 0) {
@@ -24,34 +23,93 @@ public class Account {
             id = staffList.get(staffList.size() - 1).getId() + 1;
         }
         String emailRegex = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
-        String accountRegex = "^[a-z]{3}$";
-        String phoneRegex = "^0[0-9]{8,9}$";
+        String accountRegex = "^([a-z]|[0-9]){8,16}$";
+        String phonenumberRegex = "^0[0-9]{8,9}$";
         String passwordRegex = "^[A-Za-z0-9]{8,16}$";
-//        System.out.println("Enter username: ");
-//        String username = sc.nextLine();
+
+        System.out.println("Enter username:");
+        String username;
+        boolean checkUsernameRegex;
+        while (true) {
+            username = sc.nextLine();
+            checkUsernameRegex = Pattern.matches(accountRegex, username);
+            if (checkUsernameRegex) {
+                break;
+            } else {
+                System.err.println("Wrong account pattern, enter again: ");
+            }
+        }
+
+        System.out.println("Enter password");
+        String password;
+        boolean checkPasswordRegex;
+        while (true) {
+            password = sc.nextLine();
+            checkPasswordRegex = Pattern.matches(passwordRegex, password);
+            if (checkPasswordRegex){
+                break;
+            }else {
+                System.err.println("Wrong password pattern, enter again: ");
+            }
+        }
+        System.out.println("Enter email");
+        String email;
+        boolean checkEmailRegex;
+        while (true) {
+            email = sc.nextLine();
+            checkEmailRegex = Pattern.matches(emailRegex, email);
+            if (checkEmailRegex){
+                break;
+            }else {
+                System.err.println("Wrong email pattern, enter again: ");
+            }
+        }
+
+        System.out.println("Enter phonenumber");
+        String phonenumber;
+        boolean checkPhonenumberRegex;
+        while (true) {
+            phonenumber = sc.nextLine();
+            checkPhonenumberRegex = Pattern.matches(phonenumberRegex, phonenumber);
+            if (checkPhonenumberRegex){
+                break;
+            }else {
+                System.err.println("Wrong phonenumber pattern, enter again: ");
+            }
+        }
+
+        Staff staff = new Staff(id, username, password, email, phonenumber);
+        staffList.add(staff);
+        new ConfigReadAndWrite<Staff>().writeFromFile(PATH, staffList);
+        new Main();
+    }
+
+
+    public void Login() {
+        System.out.println("Enter login username: ");
+        String username = sc.nextLine();
+        for (int i = 0; i < staffList.size(); i++) {
+            while (true){
+                if ()
+            }
+        }
+    }
+}
+
+
+
+//System.out.println("Enter login username: ");
+//        String loginname = sc.nextLine();
 //        System.out.println("Enter password: ");
-//        String password = sc.nextLine();
-//        System.out.println("Enter email: ");
-//        String email = sc.nextLine();
-//        System.out.println("Enter phone: ");
-//        String phone = sc.nextLine();
-        //boolean check = Pattern.matches()
-
-        String username = "";
-        String password = "";
-        String email = "";
-        String phone = "";
-
-       // boolean b = matcher.matches();
-Check check = new Check();
-
-        boolean checkAccountRegex = Pattern.matches(accountRegex, username);
-        boolean checkPasswordRegex = Pattern.matches(passwordRegex, password);
-        boolean checkEmailRegex = Pattern.matches(emailRegex, email);
-        boolean checkPhoneRegex = Pattern.matches(phoneRegex, phone);
+//        String loginpassword = sc.nextLine();
+//        for (int i = 0; i < staffList.size(); i++) {
+//        if (loginname.equals(staffList.get(i).getUserName()) && loginpassword.equals(staffList.get(i).getPassWord())) {
+//        new Menu();
+//        }
+//        }
 
 
-//        if (checkAccountRegex) {
+//       if (checkAccountRegex) {
 //            if (checkPasswordRegex) {
 //                if (checkEmailRegex){
 //                    if (checkPhoneRegex){
@@ -76,43 +134,3 @@ Check check = new Check();
 //            System.out.println("Wrong account pattern");
 //            new Account().Register();
 //        }
-
-        System.out.println("Enter account:");
-        username = sc.nextLine();
-        System.out.println(checkAccountRegex);
-        System.out.println(username);
-        while (!check.validate(username)) {
-            System.out.println("Wrong account pattern");
-            username = sc.nextLine();
-        }
-        while (!checkPasswordRegex) {
-            System.out.println("Wrong password pattern");
-            password = sc.nextLine();
-        }
-        while (!checkEmailRegex) {
-            System.out.println("Wrong email pattern");
-            email = sc.nextLine();
-        }
-        while (!checkPhoneRegex) {
-            System.out.println("Wrong phone pattern");
-            phone = sc.nextLine();
-        }
-        Staff staff = new Staff(id, username, password, email, phone);
-        staffList.add(staff);
-        new ConfigReadAndWrite<Staff>().writeFromFile(PATH, staffList);
-        new Main();
-
-    }
-
-    public void Login() {
-        System.out.println("Enter login username: ");
-        String loginname = sc.nextLine();
-        System.out.println("Enter password: ");
-        String loginpassword = sc.nextLine();
-        for (int i = 0; i < staffList.size(); i++) {
-            if (loginname.equals(staffList.get(i).getUserName()) && loginpassword.equals(staffList.get(i).getPassWord())) {
-                new Menu();
-            }
-        }
-    }
-}
